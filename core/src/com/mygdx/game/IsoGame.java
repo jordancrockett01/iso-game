06f8e13b179690f2316e1029ab1562d371e729f8
zsogolow <zachsogolow@msn.com>
2015-02-21 12:07:44 -0800

    Now extends Game class.

--- core/src/com/mygdx/game/IsoGame.java
+++ core/src/com/mygdx/game/IsoGame.java
@@ -1,12 +1,12 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class IsoGame extends ApplicationAdapter {
+public class IsoGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
